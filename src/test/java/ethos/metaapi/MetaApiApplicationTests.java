2ab2b054df86f5807234883695b3c7788193dfea
Thamiris-Ayumi <thamiris.ishikawa@sptech.school>
2023-11-12 19:30:57 -0300

    Criando a API de Metas

---
+++ src/test/java/ethos/metaapi/MetaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ethos.metaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
