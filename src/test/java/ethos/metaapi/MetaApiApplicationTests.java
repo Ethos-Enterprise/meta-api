21471fbc0cc2b8a45d5fe984240d6c2b84409332
Thamiris-Ayumi <thamiris.ishikawa@sptech.school>
2024-01-21 17:24:06 -0300

    feat: Adicionando mais testes unitários

--- src/test/java/ethos/metaapi/MetaApiApplicationTests.java
+++ src/test/java/ethos/metaapi/MetaApiApplicationTests.java
@@ -4,7 +4,7 @@ import ethos.metaapi.controller.MetaController;
 import ethos.metaapi.controller.request.MetaCreateRequestDto;
 import ethos.metaapi.controller.request.MetaUpdateRequestDto;
 import ethos.metaapi.controller.response.MetaResponseDto;
-import ethos.metaapi.mapper.MetaMapper;
+import ethos.metaapi.exception.MetaNotFoundException;
 import ethos.metaapi.repository.MetaRepository;
 import ethos.metaapi.repository.entity.MetaEntity;
 import ethos.metaapi.service.MetaService;
@@ -15,13 +15,12 @@ import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDate;
+import java.util.Optional;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class MetaApiApplicationTests {
@@ -43,7 +42,105 @@ class MetaApiApplicationTests {
                 LocalDate.parse("2024-10-30")
         );
     }
-    
+
+    public static MetaCreateRequestDto createMetaRequest() {
+        return new MetaCreateRequestDto(
+                "ESG",
+                "descricaoEmpresa",
+                LocalDate.parse("2024-01-10"),
+                LocalDate.parse("2024-10-30")
+        );
+    }
+
+    public static MetaUpdateRequestDto updateMetaRequest() {
+        return new MetaUpdateRequestDto(
+                "E",
+                "descricaoAtualizadaEmpresa",
+                LocalDate.parse("2024-01-10"),
+                LocalDate.parse("2026-03-20")
+        );
+    }
+
+    @Test
+    @DisplayName("Deve salvar uma nova meta")
+    void should_save_new_meta() {
+        MetaEntity entity = createMetaEntity();
+
+        when(repository.save(any(MetaEntity.class))).thenReturn(entity);
+
+        MetaResponseDto result = service.saveMeta(entity);
+
+        verify(repository, times(1)).save(any(MetaEntity.class));
+
+        assertNotNull(result);
+
+        assertEquals(entity.getId(), result.id());
+    }
+
+    @Test
+    @DisplayName("Deve atualizar uma meta existente")
+    public void should_update_existing_meta() {
+        MetaEntity updatedMeta = createMetaEntity();
+
+        updatedMeta.setDescricao("Nova descrição");
+
+        when(repository.findById(updatedMeta.getId())).thenReturn(Optional.of(updatedMeta));
+
+        when(repository.save(any(MetaEntity.class))).thenReturn(updatedMeta);
+
+        MetaResponseDto result = service.updateMeta(updatedMeta, updatedMeta.getId());
+
+        verify(repository, times(1)).findById(updatedMeta.getId());
+
+        verify(repository, times(1)).save(updatedMeta);
+
+        assertNotNull(result);
+
+        assertEquals("Nova descrição", result.descricao());
+    }
+
+    @Test
+    @DisplayName("Deve lançar exceção quando a meta não existir")
+    public void testUpdateMeta_WhenMetaDoesNotExist() {
+        MetaEntity updatedMeta = new MetaEntity();
+
+        updatedMeta.setId(UUID.randomUUID());
+
+        when(repository.findById(updatedMeta.getId())).thenReturn(Optional.empty());
+
+        MetaNotFoundException ex = assertThrows(MetaNotFoundException.class, () -> service.updateMeta(updatedMeta, updatedMeta.getId()));
+
+        assertEquals("Meta não encontrada.", ex.getMessage());
+    }
+
+    @Test
+    @DisplayName("Deve deletar meta existente")
+    public void testDeleteMeta_WhenMetaExists() {
+        UUID id = UUID.randomUUID();
+
+        when(repository.findById(id)).thenReturn(Optional.of(new MetaEntity()));
+
+        service.deleteMeta(id);
+
+        verify(repository, times(1)).findById(id);
+
+        verify(repository, times(1)).deleteById(id);
+    }
+
+    @Test
+    @DisplayName("Deve lançar exceção quando a meta não existe")
+    public void testDeleteMeta_WhenMetaDoesNotExist() {
+        UUID id = UUID.randomUUID();
+
+        when(repository.findById(id)).thenReturn(Optional.empty());
+
+        MetaNotFoundException ex = assertThrows(MetaNotFoundException.class, () -> service.deleteMeta(id));
+
+        verify(repository, never()).deleteById(id);
+
+        assertEquals(String.format("Meta com id %s não encontrada", id), ex.getMessage());
+    }
+
     @Test
     @DisplayName("Deve retornar todas as metas criadas")
     void should_return_all_metas() {
@@ -67,6 +164,7 @@ class MetaApiApplicationTests {
         MetaResponseDto meta = service.getMetaById(metaId);
 
         assertNotNull(meta);
+
         assertEquals(createMetaEntity().getId(), entity.getId());
     }
 }
